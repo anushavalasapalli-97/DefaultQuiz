c3481c729a73e9af4b64391dcce36b0d3221174e
AnushaValasapalli <anushavalasapalli@Kalyanams-MacBook-Pro.local>
2021-09-03 15:44:45 -0700

    viewcontroller missing issues

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -9,22 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblQuestion: UILabel!
+    @IBOutlet weak var lblScore: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var btnFalse: UIButton!
+    @IBOutlet weak var btnTrue: UIButton!
+    
+    
+    var quizBrain = QuizQuestionsBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        updateUI()
+        
+        
+        
+    }
+
+
+    @IBAction func btnAnswerClicked(_ sender: UIButton)  {
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+       
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateUI), userInfo: nil, repeats: true)
+        quizBrain.nextQuestion()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func updateUI() {
+        lblQuestion.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        btnTrue.backgroundColor = UIColor.clear
+        btnFalse.backgroundColor = UIColor.clear
+        lblScore.text = "Score:  \(quizBrain.getScore())"
     }
-    */
-
 }
+
